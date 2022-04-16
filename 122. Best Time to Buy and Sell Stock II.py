73ba88ebc18bcffedfa82ba4520b0a6487efae35
xieKKKi <78134664+xieKKKi@users.noreply.github.com>
2022-04-16 09:35:25 +0800

    Create 122. Best Time to Buy and Sell Stock II.py

---
+++ 122. Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        buy = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] <= buy:
+                buy = prices[i]
+            else:
+                profit += prices[i] - buy
+                buy = prices[i]
+        return profit
