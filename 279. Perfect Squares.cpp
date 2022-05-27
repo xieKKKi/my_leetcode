3b82ebe9fbb6a5d0b87db3989aa0dc0902389da6
xieKKKi <78134664+xieKKKi@users.noreply.github.com>
2022-05-27 09:37:37 +0800

    Create 279. Perfect Squares.cpp

---
+++ 279. Perfect Squares.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    //「四平方和定理」
+public:
+    // 判断是否为完全平方数
+    bool isPerfectSquare(int x) {
+        int y = sqrt(x);
+        return y * y == x;
+    }
+
+    // 判断是否能表示为 4^k*(8m+7)
+    bool checkAnswer4(int x) {
+        while (x % 4 == 0) {
+            x /= 4;
+        }
+        return x % 8 == 7;
+    }
+
+    int numSquares(int n) {
+        if (isPerfectSquare(n)) {
+            return 1;
+        }
+        if (checkAnswer4(n)) {
+            return 4;
+        }
+        for (int i = 1; i * i <= n; i++) {
+            int j = n - i * i;
+            if (isPerfectSquare(j)) {
+                return 2;
+            }
+        }
+        return 3;
+    }
+};
